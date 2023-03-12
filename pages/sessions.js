b234949c6ee55a3a1a15fb520400afbc7ea1cdf4
Brandon Kan <bkan0458@gmail.com>
2023-03-12 13:32:06 -0600

    dynamic calendar add

--- pages/sessions.js
+++ pages/sessions.js
@@ -7,11 +7,9 @@ import interaction from '@fullcalendar/interaction';
 import db from '../utils/db';
 import Session from '../models/Session';
 
-export default function SessionsScreen({sessions}) {
-  console.log(sessions)
-    const orders = [
-
-    ]
+export default function SessionsScreen({sessions, calendarSessions})  {
+    console.log(sessions)
+  const order = []
 
   
     const handleDateClick = (args) => {
@@ -22,45 +20,45 @@ export default function SessionsScreen({sessions}) {
       <FullCalendar plugins={[daygrid, interaction]}
       initialView="dayGridMonth"
       weekends={true}
-      events={[
-        {title: 'Calebs Tutoring Class', date:'2023-03-11'},
-        {title: 'Calebs Tutoring Class', date:'2023-03-15'},
-      ]}
+      events={
+        calendarSessions
+      }
      
       dateClick= {handleDateClick}/>
-      <div className="overflow-x-auto">
+      <div className="overflow-x-auto p-3 border-2 mt-2">
         <table className="min-w-full">
           <thead className="border-b">
             <tr>
-              <th className="px-5 text-left">ID</th>
-              <th className="px-5 text-left">Date</th>
-              <th className="px-5 text-left">Total</th>
+              <th className="px-5 text-left">Student</th>
+              <th className="px-5 text-left">Date of Class</th>
+              <th className="px-5 text-left">Subject</th>
+              <th className="px-5 text-left">Attended</th>
               <th className="px-5 text-left">Paid</th>
-              <th className="px-5 text-left">Delivered</th>
-              <th className="px-5 text-left">Action</th>
             </tr>
           </thead>
 
           <tbody>
-            {orders.map((order) => (
+            {sessions.map((session) => (
               <tr key={order._id} className="border-b">
-                <td className="p-5">{order._id.substring(20, 24)}</td>
-                <td className="p-5">{order.createdAt.substring(0, 10)}</td>
-                <td className="p-5">${order.totalPrice}</td>
+                <td className="p-5">{session.student}</td>
+                <td className="p-5">{session.date.substring(0, 10)}</td>
+                <td className="p-5">{session.subject}</td>
                 <td className="p-5">
-                  {order.isPaid
-                    ? `${order.paidAt.substring(0, 10)}`
+                  {session.attended ?
+                  'Attended' :
+                  'Not attended'}
+                </td>
+                <td className="p-5">
+                  {session.isPaid
+                    ? 'paid'
                     : 'not paid'}
                 </td>
                 <td className="p-5">
-                  {order.isDelivered
-                    ? `${order.deliveredAt.substring(0, 10)}`
-                    : 'not delivered'}
-                </td>
-                <td className="p-5">
-                  <Link href={`/order/${order._id}`} passHref>
+                  {/* This will open a modal */}
+                  <Link href="/">Details</Link>
+                  {/* <Link href={`/order/${order._id}`} passHref>
                     Details
-                  </Link>
+                  </Link> */}
                 </td>
               </tr>
             ))}
@@ -72,11 +70,23 @@ export default function SessionsScreen({sessions}) {
 }
 
 export async function getServerSideProps() {
+  const calendarSessions = []
+  
   await db.connect();
   const sessions = await Session.find({}).lean();
+  for (var i = 0; i < sessions.length; i++) {
+
+    let newDateFormat = new Date(sessions[i].date).toISOString();
+    let sessionObj = {
+      title: sessions[i].student,
+      date: newDateFormat
+    }
+    calendarSessions.push(sessionObj);
+  }
   return {
     props: {
       sessions: sessions.map(db.convertDocToObj),
+      calendarSessions
     },
   };
 }
--- utils/db.js
+++ utils/db.js
@@ -34,10 +34,12 @@ async function disconnect() {
 
 function convertDocToObj(doc) {
   doc._id = doc._id.toString();
-  doc.date = doc.date.toString();
   if (doc.createdAt || doc.updatedAt) {
     doc.createdAt = doc.createdAt.toString();
     doc.updatedAt = doc.updatedAt.toString();
+  }
+  if (doc.date) {
+    doc.date = doc.date.toString();
   }
 
   return doc;
